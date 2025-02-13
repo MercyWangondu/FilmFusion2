7666040315c83c74c15a6ee613e0d6a64f36a680
Wangondu, Mercy Mumbi <132012844+MercyWangondu@users.noreply.github.com>
2025-02-13 19:03:24 +0300

    This contains the source folder

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "0a425e84ed65e3db302986d85cc80f8f";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
